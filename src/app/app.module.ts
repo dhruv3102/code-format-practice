1f06ea041d4c18e556e8f9722e047e18433fb145
dhruv3102 <dhruv.s@keka.com>
2023-06-08 15:36:12 +0530

    added documentation and component login-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, LoginPageComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent]
