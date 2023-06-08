11f0654aed3d040947dce426b3c3b78618674697
dhruv3102 <dhruv.s@keka.com>
2023-06-08 15:41:42 +0530

    home-page component is created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
-	declarations: [AppComponent, LoginPageComponent],
+	declarations: [AppComponent, LoginPageComponent, HomePageComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent]
