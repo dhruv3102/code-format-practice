7949698099229da29e222edd02361113df462506
dhruv3102 <dhruv.s@keka.com>
2023-06-08 17:54:39 +0530

    created a model and modify homepage

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 
+import { Contact } from '../models/contact';
+
 @Component({
 	selector: 'app-home-page',
 	templateUrl: './home-page.component.html',
 	styleUrls: ['./home-page.component.sass']
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+	contactList: Contact[] = [];
+
+	getContactDetails(): Contact {
+		// return contact
+		return this.contactList['mobile'];
+	}
+}
---
+++ src/app/models/contact.ts
@@ -0,0 +1,8 @@
+export class Contact {
+	mobile: string;
+
+	constructor(args) {
+		//
+		this.mobile = args.mobile;
+	}
+}
